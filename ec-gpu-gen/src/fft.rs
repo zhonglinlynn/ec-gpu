5706df81e4584f4427121fce54005bcbe790c643
lin <853077855@qq.com>
2022-08-03 15:55:07 +0800

    pairing

--- ec-gpu-gen/src/fft.rs
+++ ec-gpu-gen/src/fft.rs
@@ -252,8 +252,8 @@ mod tests {
 
     use pairing_ce::ff::{Field, PrimeField};
     //use pairing_ce::bn256::Bn256;
-    //use pairing_ce::compact_bn256::Bn256;
-    use pairing_ce::bls12_381::Bls12 as Bn256;
+    use pairing_ce::compact_bn256::Bn256;
+    //use pairing_ce::bls12_381::Bls12 as Bn256;
     use rand::{thread_rng, Rng};
     use pairing_ce::ff::ScalarEngine;
 
@@ -272,7 +272,7 @@ mod tests {
     }
 
     #[test]
-    pub fn gpu_fft_consistency() {
+    pub fn gpu_fft() {
         let rng = &mut thread_rng();
 
         let worker = Worker::new();
